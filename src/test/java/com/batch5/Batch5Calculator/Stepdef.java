66dd0eade30c559b66d09c003c21391664a891eb
DurgaPrasad126 <durgaprasadalakunta9@gmail.com>
2019-07-24 14:49:05 +0530

    updated the Stepdef and cucumbertest

---
+++ src/test/java/com/batch5/Batch5Calculator/Stepdef.java
@@ -0,0 +1,28 @@
+package com.batch5.Batch5Calculator;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdef {
+	
+	@Given("Rest Api {string}")
+	public void rest_Api(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I invoke the given rest api")
+	public void i_invoke_the_given_rest_api() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the expected result is {double}")
+	public void the_expected_result_is(Double double1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+
+}
