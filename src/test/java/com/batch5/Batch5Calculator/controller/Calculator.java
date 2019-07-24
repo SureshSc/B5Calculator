955ccc85c411eddd98864ac7b5a9133b0f763f68
Waris <mr.warisquamer@gmail.com>
2019-07-24 06:47:57 +0530

    Updated multiplication feature

---
+++ src/test/java/com/batch5/Batch5Calculator/controller/Calculator.java
@@ -0,0 +1,17 @@
+package com.batch5.Batch5Calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calculator {
+	
+	@GetMapping("multiplication")
+	@ResponseBody
+	public double multiplication(@RequestParam double firstOperand, @RequestParam double secondOperand) {
+		return firstOperand * secondOperand;
+	}
+
+}
